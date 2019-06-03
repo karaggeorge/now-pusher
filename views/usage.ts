a063b9efda80edfea1646fa3de6a42095f0016b5
Zan Markan <zan.markan@gmail.com>
2019-06-03 11:31:26 +0100

    Some minor copy suggestions to clarify this is specifically for Pusher Channels product.

--- views/usage.ts
+++ views/usage.ts
@@ -10,13 +10,13 @@ const UsageView = ({utils}: RouteOptions) => {
     <Fieldset>
       <FsContent>
         <FsTitle>Server</FsTitle>
-        <P>Add the pusher package to your project</P>
+        <P>Add the Pusher Channels server SDK to your project</P>
         <Code value=${encodeURIComponent('$ yarn add pusher')}/>
         <BR/>
         <P>Then, add the code below to your server</P>
         <Code lang="syntax" value=${encodeURIComponent(nodeCode)}/>
         <BR/>
-        <P>The <Link href="https://github.com/pusher/pusher-http-node" target="_blank">docs</Link> contain more information on how to use the client</P>
+        <P>The <Link href="https://pusher.com/docs/channels/server_api/overview" target="_blank">docs</Link> contain more information on how to use the SDK</P>
       </FsContent>
       <FsFooter>
         The environment variables used above have already been added to your project
@@ -25,7 +25,7 @@ const UsageView = ({utils}: RouteOptions) => {
     <Fieldset>
       <FsContent>
         <FsTitle>Client</FsTitle>
-        <P>Add the pusher package to your project</P>
+        <P>Add the Pusher Channels client SDK to your project</P>
         <Code value=${encodeURIComponent('$ yarn add pusher-js')}/>
         <P>or</P>
         <Code value=${encodeURIComponent('<script src="https://js.pusher.com/4.4/pusher.min.js"></script>')}/>
@@ -33,7 +33,7 @@ const UsageView = ({utils}: RouteOptions) => {
         <P>Then, add the code below to your client</P>
         <Code lang="syntax" value=${encodeURIComponent(javascriptCode(keys))}/>
         <BR/>
-        <P>The <Link href="https://pusher.com/docs/channels/getting_started/javascript" target="_blank">docs</Link> contain more information on how to use the client</P>
+        <P>The <Link href="https://pusher.com/docs/channels/getting_started/javascript" target="_blank">docs</Link> contain more information on how to use the SDK</P>
       </FsContent>
     </Fieldset>
   `;
